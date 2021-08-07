c3b6c08fd3862d0dc43bd7256466cccc75db59f8
Jack Linhart <iatenine@gmail.com>
2021-08-06 21:53:45 -0600

    Added hours of day and reusable function to append each hour

--- Assets/app.js
+++ Assets/app.js
@@ -1,1 +1,41 @@
-console.log("hello");
+// Save day of week from moment to const
+const dayOfWeek = moment().format("dddd MMM Do YYYY"); // Sunday, March 14th 2016 (or something like that)
+const busHours = [];
+
+$("#currentDay").append("<div class=>" + dayOfWeek + "</div>");
+
+function init() {
+  for (var i = 9; i < 18; i++) {
+    busHours.push(moment().hour(i).format("HH") + ":00");
+  }
+  busHours.forEach((elem) => {
+    addElementToDayPlanner(elem);
+  });
+}
+init();
+console.log(busHours);
+
+function addElementToDayPlanner(time) {
+  // Needs a label, text input area and a button
+  const divWrapper = $("<div>");
+  const planner = $("#day-planner");
+  const label = $("<label>");
+  const input = $("<input>");
+  const button = $("<button>");
+  label.html(time);
+  input.attr("type", "text");
+  input.attr("data-index", time);
+  button.html("Add");
+  button.attr("data-index", time);
+  divWrapper.append(label);
+  divWrapper.append(input);
+  divWrapper.append(button);
+  divWrapper.on("click", "button", handleButtonClick);
+  planner.append(divWrapper);
+}
+
+function handleButtonClick(e) {
+  console.log("button clicked", e);
+  console.log(e.currentTarget);
+  console.log(e.currentTarget.dataset.index);
+}
